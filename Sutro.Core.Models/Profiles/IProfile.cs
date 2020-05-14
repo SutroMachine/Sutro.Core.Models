617d86e182f317653f035709c4dee00d5cc7a99c
Greg Meess <greg@sutromachine.com>
2020-05-14 16:09:11 -0700

    Add IProfile combining machine, material & part

---
+++ Sutro.Core.Models/Profiles/IProfile.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sutro.Core.Models.Profiles
+{
+    public interface IProfile : IMachineProfile, IMaterialProfile, IPartProfile
+    {
+    }
+}
