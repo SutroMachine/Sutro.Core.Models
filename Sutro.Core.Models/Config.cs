d327fb30d49c3693d9f7f456b8815952d6480b5d
Greg Meess <greg@sutromachine.com>
2020-06-19 17:56:17 -0700

    Add Config class for setting parallel, debug, etc. across all libraries

---
+++ Sutro.Core.Models/Config.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+namespace Sutro.Core.Models
+{
+    public static class Config
+    {
+        public static bool DebuggerAttached { get; set; }        
+        public static bool Debug { get; set; }
+        public static bool Parallel { get; set; }
+    }
+}
