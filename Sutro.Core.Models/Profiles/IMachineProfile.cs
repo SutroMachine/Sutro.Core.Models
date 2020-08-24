31c5512fa181e4d11d4b42367ce4257444bffbbe
Greg Meess <greg@sutromachine.com>
2020-08-24 12:00:05 -0700

    Simplify names for bed size

--- Sutro.Core.Models/Profiles/IMachineProfile.cs
+++ Sutro.Core.Models/Profiles/IMachineProfile.cs
@@ -1,13 +1,19 @@
-﻿namespace Sutro.Core.Models.Profiles
+﻿using System;
+
+namespace Sutro.Core.Models.Profiles
 {
     public interface IMachineProfile : IProfile
     {
         string ManufacturerName { get; set; }
         string ModelIdentifier { get; set; }
 
-        double MachineBedSizeXMM { get; set; }
-        double MachineBedSizeYMM { get; set; }
-        double MachineBedSizeZMM { get; set; }
+        double BedSizeXMM { get; set; }
+        double BedSizeYMM { get; set; }
+
+        [Obsolete("Use MaxHeightMM instead")]
+        double BedSizeZMM { get; set; } 
+
+        double MaxHeightMM { get => BedSizeZMM; set => BedSizeZMM = value; }
 
         MachineBedOriginLocationX OriginX { get; set; }
         MachineBedOriginLocationY OriginY { get; set; }
